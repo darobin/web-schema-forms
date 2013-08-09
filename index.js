e4a5a58a298ea395aad31c01caf2d5dddf01b348
Robin Berjon <robin@berjon.com>
2013-08-09 15:09:55 +0200

    refactor inheritance and encapsulation

--- index.js
+++ index.js
@@ -4,13 +4,6 @@ var swig = require("swig")
 ,   fs = require("fs")
 ;
 
-// TODO
-// configure an inheritance line so that templates can specialise their parent
-// there is a root template that essentially does nothing except walk the schema tree
-// and call the right inner templates
-// precompile templates?
-
-
 // register sources and all of their templates
 var sources = {};
 
@@ -26,18 +19,40 @@ exports.register = function (name, path) {
 exports.register("root", pth.resolve(pth.join(__dirname, "root")));
 exports.register("basic", pth.resolve(pth.join(__dirname, "basic")));
 
-// handle inheritance
-exports.inheritance = ["root", "basic"];
+// helper
+function isArray (obj) {
+    return Object.prototype.toString.call(obj) === "[object Array]";
+}
 
-exports.init = function () {
-    if (exports.inheritance[0] !== "root") exports.inheritance.unshift("root");
+exports.form = function (inheritance, schema, hints) {
+    // the first parameter is optional
+    if (!isArray(inheritance)) {
+        schema = inheritance;
+        hints = schema;
+        inheritance = ["root", "basic"];
+    }
+    if (!schema) throw new Error("Can't convert schema if no schema is given.");
+    if (!hints) hints = {};
+    if (inheritance[0] !== "root") inheritance.unshift("root");
     swig.init({
         extensions: {
-            isArray:    function (obj) {
-                return Object.prototype.toString.call(obj) === "[object Array]";
-            }
+            isArray: isArray
         }
     });
+
+    // finds what template to inherit from
+    function findInherit (start, file, want) {
+        while (start > 0) {
+            start--;
+            if (sources[inheritance[start]].html[file]) {
+                if (want === "file") return inheritance[start] + "/" + file;
+                if (want === "html") return sources[inheritance[start]].html[file];
+                if (want === "template") return sources[inheritance[start]].templates[file];
+                if (want === "source") return inheritance[start];
+            }
+        }
+        return null;
+    }
 
     // macros
     fs.readdirSync(pth.join(__dirname, "macros"))
@@ -45,43 +60,32 @@ exports.init = function () {
           if (!/\.html$/.test(it)) return;
           swig.compile(fs.readFileSync(pth.join(__dirname, "macros", it), "utf8"), { filename: "macros/" + it });
       });
-    
-    // XXX
-    //  this will crash if there's nothing at the previous level
-    //  instead:
-    //      - look through all previous levels and use the first
-    //      - if there are none, don't extend
-    var previousLevel;
-    for (var i = 0, n = exports.inheritance.length; i < n; i++) {
-        var level = exports.inheritance[i];
-        for (var k in sources[level].html) {
-            var content = previousLevel ? "{% extends '" + previousLevel + "/" + k + "' %}" : "";
-            content += sources[level].html[k];
-            sources[level].templates[k] = swig.compile(content, { filename: level + "/" + k});
-        }
-        previousLevel = level;
-    }
-};
 
-exports.form = function (schema, hints) {
-    if (!hints) hints = {};
-    // start with schema.html template that's last in inheritance
-    var tpl, idx = exports.inheritance.length;
-    while (!tpl && idx > 0) {
-        idx--;
-        tpl = sources[exports.inheritance[idx]].templates["schema.html"];
-    }
-    var inheritanceTop = {};
-    for (var k in sources.root.html) {
-        var idx = exports.inheritance.length;
-        while (idx > 0) {
-            idx--;
-            if (sources[exports.inheritance[idx]].html[k]) {
-                inheritanceTop[k.replace(".html", "")] = exports.inheritance[idx] + "/" + k;
-                break;
-            }
+    // for each template from each source, make it inherit from its real parent (if any)
+    for (var idx = 0, n = inheritance.length; idx < n; idx++) {
+        var src = sources[inheritance[idx]];
+        for (var k in src.html) {
+            var parent = findInherit(idx, k, "file")
+            ,   content = (parent ? "{% extends '" + parent + "' %}" : "") + src.html[k]
+            ;
+            src.templates[k] = swig.compile(content, { filename: inheritance[idx] + "/" + k});
         }
     }
+
+    // start with schema.html template that's last in inheritance
+    var tpl = findInherit(inheritance.length, "schema.html", "template");
     if (!tpl) throw new Error("No schema.html template in inheritance tree.");
+
+    // need to list all templates at all levels, and for each find the topmost
+    var inheritanceTop = {}, allHTML = {}, listHTML = [];
+    for (var i = 0, n = inheritance.length; i < n; i++) {
+        for (var k in sources[inheritance[i]].html) allHTML[k] = true;
+    }
+    for (var k in allHTML) listHTML.push(k);
+    for (var i = 0, n = listHTML.length; i < n; i++) {
+        var html = listHTML[i];
+        inheritanceTop[html.replace(".html", "")] = findInherit(inheritance.length, html, "file");
+    }
+
     return tpl({ schema: schema, inheritanceTop: inheritanceTop, hints: hints });
 };
